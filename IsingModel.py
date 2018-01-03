98f1436a5b953638092979402419e1b239a552fa
Josh Taylor <taylorj5@tcd.ie>
2018-01-03 02:13:04 +0000

    Added the algorithm to plot magnetisation
    The Metropolis algorithm now iterates over the lattice in order to bring it to equilibrium. Then it iterates again in order to measure the average value of a physical property of the system, here the magnetisation. The program does not yet yield the correct value for the critical temperature, but further work will be done tomorrow.

--- IsingModel.py
+++ IsingModel.py
@@ -3,9 +3,6 @@ import numpy as np
 from numpy.random import rand
 import scipy.constants
 
-T = 273.14
-k = scipy.constants.k
-beta = 1.0/k*T
 
 #defining function to randomly generate initial spin configuration
 def initialise(n):
@@ -18,7 +15,7 @@ def metropolis(state,beta,n):
     b = np.random.randint(0,n)
     spin_ij = state[a,b] #considering the spin of a random point in the lattice
     spin_neighbours = state[(a+1)%n,b] + state[(a-1)%n,b] + state[a,(b+1)%n] + state[a,(b+1)%n] #considering spin of the neighbours #by using modulo n, periodic boundary conditions can be established
-    hamil = -spin_ij*spin_neighbours - spin_ij
+    hamil = -spin_ij*spin_neighbours #- spin_ij
     dE = -2*hamil #as dE = flipped Hamiltonian - Hamiltonian = -2*Hamiltonian
     if dE < 0:
         spin_ij *= -1.0
@@ -29,31 +26,51 @@ def metropolis(state,beta,n):
     return state
 
 #function that plots the current spin state of the lattice when called
-def plot(result,i):
+def plot(state,i):
     plt.figure(figsize=(5,5))
-    plt.imshow(result)
+    plt.imshow(state)
     plt.title("n = %s" %i)
-    
-#main function                      
-n = 100
-nsteps = 200000
 
-state = initialise(n)
-print (state)
+#simple function to return total magnetisation of the lattice
+def find_mag(state):
+    mag = np.sum(state)
+    return mag
+
+
+#defining necessary variables etc
+k = scipy.constants.k
+T_arr = np.linspace(1.1,4.0,58)
+T = T_arr/k
+
+n = 16
+eqsteps = 1000000  #number of steps to bring lattice to equilibrium
+calcsteps = 500000 #number of steps for calculation of thermodynamic properties
+magnetisation = []
+
+#main part of the code
 
 #loop to repeatedly apply the algorithm to the lattice
-for i in range(nsteps):  
-    result = metropolis(state,beta,n)
-    if i == 1:
-        plot(result,i)
-    if i == 1000:
-        plot(result,i)
-    if i == 5000:
-        plot(result,i)
-    if i == 10000:
-        plot(result,i)
-    if i == 100000:
-        plot(result,i)
+for j in range(len(T)):
+    mag = 0
+    state = initialise(n)
+    beta = 1/(k*T[j])
     
-print (result)
-plot(result,nsteps)
+    for i in range(eqsteps): #system is brought to equilbrium 
+        metropolis(state,beta,n)
+            
+    for i in range(calcsteps):
+        metropolis(state,beta,n)
+        mag += find_mag(state)
+    magnetisation.append(mag/(calcsteps*n*n))
+    
+print(magnetisation)
+fig = plt.plot(T,np.fabs(magnetisation),'ro')
+plt.xlabel('Temperature (T)')
+plt.ylabel('Magnetisation')
+plt.show()
+    
+    
+    
+    
+    
+    
