d607d85992bd765eb865a36cfc414c6b0ef84dc9
Josh Taylor <taylorj5@tcd.ie>
2018-01-04 03:39:07 +0000

    Added function to calculate energy & amended code to calculate various properties and plot them for a range of temperatures
    The program now calculates energy, magnetisation, susceptibility and specific heat of the system, and plots them for a range of values near the critical point

--- magnetization.py
+++ magnetization.py
@@ -29,7 +29,16 @@ def metropolis(state, beta):
 def find_mag(state):
     mag = np.sum(state)
     return mag    
-       
+  
+
+def find_energy(state):
+    energy = 0
+    for i in range(0,n):
+        for j in range(0,n):
+            spin_ij = state[i,j]
+            neighbours = state[(i+1)%n,j] + state[(i-1)%n,j] + state[i,(j+1)%n] + state[i, (j-1)%n]
+            energy += -spin_ij*neighbours
+    return energy
     
 #defining necessary variables etc
 T = np.random.normal(2.26,0.5,2**5) #creating normal distribution around T_c to easier see change in magnetisation
@@ -38,33 +47,64 @@ T = T[(T>1.1) & (T<3.8)] #taking only values of T in a certain range
 n = 2**4
 eqsteps = 500000 #number of steps to bring lattice to equilibrium
 compsteps = 100000 #number of steps for calculation of thermodynamic properties
-magnetisation = []
+magnetisation = energy = susceptibility = specific_heat = []
+
 
 #main part of the code
 start_time = time.time() #timing code out of interest
 
 #loop to repeatedly apply the algorithm to the lattice
 for j in range(len(T)):
-    mag = 0
+    mag = ene = sus = spe = 0
     state = initialise_array(n)
-    b = 1.0/T[j]
+    beta = 1.0/T[j]
     
     for i in range(eqsteps): #system is brought to equilbrium 
-        metropolis(state,b)
+        metropolis(state,beta)
             
     for i in range(compsteps):
-        metropolis(state,b)
-        m1 = find_mag(state)
-        mag += m1
+        metropolis(state,beta)
+        m = find_mag(state)
+        e = find_energy(state)
+        mag += m
+        ene += e
+        sus += m**2
+        spe += e**2
+        
     magnetisation.append(mag/(compsteps*n*n))
-    
+    energy.append(ene/(compsteps*n*n))
+    susceptibility.append(((mag/(compsteps*n*n))-(((mag)**2)/(compsteps*compsteps*n*n)))*beta)
+    specific_heat.append(((ene/(compsteps*n*n))-(((ene)**2)/(compsteps*compsteps*n*n)))*(beta**2))
+
 print ("for loop took", (time.time() - start_time)/60, "minutes")
 
+fig = plt.figure()
+
+fig.add_subplot(122)
 plt.plot(T,np.fabs(magnetisation),'ro')
 plt.title('Magnetization against Temperature')
 plt.xlabel('Temperature (T) in J/k')
 plt.ylabel('Magnetisation')
 plt.ylim(-0.05,1.05)
+
+fig.add_subplot(222)
+plt.plot(T,np.fabs(energy),'ko')
+plt.title('Energy against Temperature')
+plt.xlabel('Temperature (T) in J/k')
+plt.ylabel('Energy')
+
+fig.add_subplot(322)
+plt.plot(T,np.fabs(susceptibility),'ro')
+plt.title('Susceptibility against Temperature')
+plt.xlabel('Temperature (T) in J/k')
+plt.ylabel('Susceptibility')
+
+fig.add_subplot(422)
+plt.plot(T,np.fabs(specific_heat),'ko')
+plt.title('Specific Heat against Temperature')
+plt.xlabel('Temperature (T) in J/k')
+plt.ylabel('Specific Heat')
+
 plt.show()
     
     
